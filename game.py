2c5e09ee3f4fb363c9ab8d93f9c18bc57d5b78c9
Tobias <tobias.kulb@web.de>
2024-06-14 08:24:27 +0200

    ritter commit

--- game.py
+++ game.py
@@ -1,53 +1,52 @@
 import random
 
-
-
 def play():
+    number = random.randint(1, 100)
+    print(f"Die gesuchte Zahl ist: {number}")
+    user_wins = False
+    score = 0
+
+    while not user_wins:
+        score += 1
+
+        try:
+            guess = int(input("Geben Sie eine Zahl zwischen 1 und 100 ein: "))
+            if guess < 1 or guess > 100:
+                raise ValueError
+        except ValueError:
+            print("Falsche Eingabe!")
+            continue
+
+        if number == guess:
+            print(f"Yes, {number} ist die gesuchte Zahl. Score: {score}")
+            user_wins = True
+        elif guess > number:
+            print("Die gesuchte Zahl ist kleiner.")
+        else:
+            print("Die gesuchte Zahl ist größer.")
+
+    save_score(score)
+
 
 def save_score(score):
-#highscore
-    pass
+    try:
+        with open("scores.txt", "a") as file:
+            file.write(f"Score: {score}\n")
+        print("Score erfolgreich gespeichert!")
+    except Exception as e:
+        print(f"Fehler beim Speichern des Scores: {e}")
 
-
-    while True:
-        number = random.randint(1, 100)
-        print(f"DEBUG: number ist {number}")
-        user_wins = False
-        anzahl = 0
-
-        while not user_wins:
-            anzahl += 1
-            try:
-                guess = int(input("Geben Sie eine Zahl zwischen 1 und 100 ein: "))
-                if guess < 1 or guess > 100:
-                    raise ValueError
-            except ValueError:
-                print("Ungültige Eingabe! Bitte geben Sie eine Zahl zwischen 1 und 100 ein.")
-                continue
-
-            if number == guess:
-                print(f"Ja, {number} ist die richtige Zahl! Du hast {anzahl} Versuche gebraucht.")
-                user_wins = True
-                print(f"Dein Highscore ist {anzahl}")
-            elif number > guess:
-                print("Die Zahl ist zu klein.")
-            else:
-                print("Die Zahl ist zu groß.")
-
-#    restart = input("Möchten Sie das Spiel neu starten? (ja/nein): ").strip().lower()
-#    if restart != 'ja':
-#        print("Danke fürs Spielen! Auf Wiedersehen!")
-#        break
 
 while True:
     play()
 
     correctUserInput = False
-    while correctUserInput:
-        prompt = input("Wollen Sie noch eine Runde spielen y/n?")
-        if prompt == "n":
+    while not correctUserInput:
+        prompt = input("Wollen Sie noch eine Runde spielen? [y/n] ")
+
+        if prompt == 'n':
             exit()
-        elif prompt == "y":
+        elif prompt == 'y':
             correctUserInput = True
         else:
-            print("falsche eingabe nur y oder n erlaubt")
+            print("Falsche Eingabe, nur 'y' oder 'n' erlaubt.")
